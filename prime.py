2cbb90a3682fa69bf73c0e82a4548d11e44afad4
Salatiel Souza <sal.souzasa@gmail.com>
2020-10-14 17:28:46 -0300

    changes names

--- prime.py
+++ prime.py
@@ -3,20 +3,20 @@ def generatorPrime():
     init = int(input("Enter first value: "))
     final = int(input("Enter last value: "))
     
-    lista = []
-    myList = []
+    list_resumed = []
+    my_list = []
 
     for x in range(init, final):
         lista.append(x)
         lista
 
-    for number in lista:
+    for number in list_resumed:
         if number > 1:
             for i in range(2, number):
                 if number % i == 0:
                     break
             else:
-                myList.append(number)
+                my_list.append(number)
 
     print("This sequence of prime numbers:", myList)
 
